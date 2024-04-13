c0b352747681ad02904bc41507113a3728119956
RFC1920 <desolationoutpostpve@gmail.com>
2024-04-13 14:11:56 -0500

    add license

--- Photog.cs
+++ Photog.cs
@@ -1,4 +1,23 @@
-using System;
+#region License (GPL v2)
+/*
+    DESCRIPTION
+    Copyright (c) 2024 RFC1920 <desolationoutpostpve@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License v2.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+#endregion License Information (GPL v2)
 using System.Collections.Generic;
 using Oxide.Core;
 using Oxide.Core.Libraries.Covalence;
